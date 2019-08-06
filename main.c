43068e850ddc98e4b96548a54447963ba25af525
Arina Okshus <45241701+chameleon-lizard@users.noreply.github.com>
2019-08-06 13:24:29 +0000

    Better interface, still no encryprion

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
+#include <gst/video/videooverlay.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <gst/gst.h>
+#include <gdk/gdkx.h>
 #include <gtk/gtk.h>
+#include <gst/gst.h>
+#include <gdk/gdk.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <malloc.h>
@@ -12,6 +15,7 @@
 
 int sock = 0;
 int valread;
+
 char *oip;
 char *olp;
 char *sip;
@@ -25,8 +29,30 @@ enum
     PORT = 3312
 };
 
-int
-opendoor(GtkWidget *widget, gpointer data)
+/* Structure to contain all our information, so we can pass it around */
+typedef struct _CustomData {
+    GstElement *playbin;           /* Our one and only pipeline */
+
+    GtkWidget *slider;              /* Slider widget to keep track of current position */
+    GtkWidget *streams_list;        /* Text widget to display info about the streams */
+    gulong slider_update_signal_id; /* Signal ID for the slider update signal */
+
+    GstState state;                 /* Current state of the pipeline */
+    gint64 duration;                /* Duration of the clip, in nanoseconds */
+} CustomData;
+
+static void realize_cb (GtkWidget *widget, CustomData *data) 
+{
+    GdkWindow *window = gtk_widget_get_window (widget);
+    guintptr window_handle;
+
+    window_handle = GDK_WINDOW_XID (window);
+
+    gst_video_overlay_set_window_handle (GST_VIDEO_OVERLAY (data->playbin), window_handle);
+}
+
+/* This function is called when the PLAY button is clicked */
+static void dooropen (GtkButton *button, CustomData *data) 
 {
     char allowpass[] = "ALLOWPASS 1 ANONYMOUS IN\r\n";
 
@@ -34,7 +60,7 @@ opendoor(GtkWidget *widget, gpointer data)
     char return_value[RETURN_VALUE_SIZE] = {0};
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("Socket not created. Terminating application\n");
-        return -1;
+        return;
     }
 
     serv_addr.sin_family = AF_INET;
@@ -42,12 +68,12 @@ opendoor(GtkWidget *widget, gpointer data)
 
     if (inet_pton(AF_INET, oip, &serv_addr.sin_addr) <= 0) {
         printf("Invalid config\n");
-        return -1;
+        return;
     }
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         printf("Connection Failed\n");
-        return -1;
+        return;
     }
 
     char *login = (char *)calloc(strlen("LOGIN 1.8 \r\n") + strlen(olp) + 1, sizeof(*login));
@@ -62,7 +88,7 @@ opendoor(GtkWidget *widget, gpointer data)
         printf("Login failed\n");
     } else {
         // Allowing pass
-         
+
         send(sock, allowpass, strlen(allowpass), 0);
         sleep(1); // ???
 
@@ -75,31 +101,72 @@ opendoor(GtkWidget *widget, gpointer data)
         }
     }
 
-
     free(login);
-
-    return 0;
 }
 
-void destroy(GtkWidget *widget, gpointer data )
+/* This function is called when the main window is closed */
+static void delete_event_cb (GtkWidget *widget, GdkEvent *event, CustomData *data) 
 {
-    gtk_main_quit();
+    gtk_main_quit ();
 }
 
-int
-main(int argc, char *argv[])
+static void create_ui (CustomData *data) 
 {
-    GtkWidget *window;
-    GtkWidget *button;
-    gtk_init(&argc, &argv);
+    GtkWidget *main_window;  /* The uppermost window, containing all other windows */
+    GtkWidget *video_window; /* The drawing area where the video will be shown */
+    GtkWidget *main_box;     /* VBox to hold main_hbox and the controls */
+    GtkWidget *main_hbox;    /* HBox to hold the video_window */
+    GtkWidget *controls;     /* HBox to hold the buttons and the slider */
+    GtkWidget *open_button; /* Buttons */
 
-    GstElement *pipeline;
+    main_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    g_signal_connect (G_OBJECT (main_window), "delete-event", G_CALLBACK (delete_event_cb), data);
+
+    video_window = gtk_drawing_area_new ();
+    g_signal_connect (video_window, "realize", G_CALLBACK (realize_cb), data);
+
+    open_button = gtk_button_new_from_icon_name ("go-next", GTK_ICON_SIZE_SMALL_TOOLBAR);
+    g_signal_connect (G_OBJECT (open_button), "clicked", G_CALLBACK (dooropen), data);
+
+    controls = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_box_pack_start (GTK_BOX (controls), open_button, FALSE, FALSE, 2);
+
+    main_hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_box_pack_start (GTK_BOX (main_hbox), video_window, TRUE, TRUE, 0);
+    gtk_box_pack_start (GTK_BOX (main_hbox), data->streams_list, FALSE, FALSE, 2);
+
+    main_box = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
+    gtk_box_pack_start (GTK_BOX (main_box), main_hbox, TRUE, TRUE, 0);
+    gtk_box_pack_start (GTK_BOX (main_box), controls, FALSE, FALSE, 0);
+    gtk_container_add (GTK_CONTAINER (main_window), main_box);
+    gtk_window_set_default_size (GTK_WINDOW (main_window), 1280, 750);
+
+    gtk_widget_show_all (main_window);
+}
+
+int main(int argc, char *argv[]) 
+{
+    CustomData data;
+    GstStateChangeReturn ret;
     GstBus *bus;
-    GstMessage *msg;
 
-    int pid = 0;
+    /* Initialize GTK */
+    gtk_init (&argc, &argv);
 
-     // Reading the opener IP address, login and password from the config
+    /* Initialize GStreamer */
+    gst_init (&argc, &argv);
+
+    memset (&data, 0, sizeof (data));
+    data.duration = GST_CLOCK_TIME_NONE;
+
+    data.playbin = gst_element_factory_make ("playbin", "playbin");
+
+    if (!data.playbin) {
+        g_printerr ("Not all elements could be created.\n");
+        return -1;
+    }
+
+    // Reading the opener IP address, login and password from the config
     FILE *config = fopen("config", "r");
     char *cfg = (char *)calloc(CFG_MAX, sizeof(*cfg));
 
@@ -114,36 +181,24 @@ main(int argc, char *argv[])
     *strstr(oip, "\n") = 0;
     *strstr(olp, "\n") = 0;
 
-    if (!(pid = fork())) {
-        window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_object_set (data.playbin, "uri", sip, NULL);
 
-        g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
-        gtk_container_set_border_width(GTK_CONTAINER(window), 1);
-        button = gtk_button_new_with_label("Open door");
-        g_signal_connect(GTK_OBJECT(button), "clicked", G_CALLBACK(opendoor), "button");
-        gtk_container_add(GTK_CONTAINER(window), button);
-        gtk_widget_show_all(window);
-        gtk_main();
-    } else {
-        gst_init(&argc, &argv);
-        pipeline = gst_parse_launch(sip, NULL);
-        gst_element_set_state(pipeline, GST_STATE_PLAYING);
-        bus = gst_element_get_bus(pipeline);
-        msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+    create_ui (&data);
+
+    ret = gst_element_set_state (data.playbin, GST_STATE_PLAYING);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        g_printerr ("Unable to set the pipeline to the playing state.\n");
+        gst_object_unref (data.playbin);
+        return -1;
     }
 
-    while (wait(NULL) != -1);
+    /* Start the GTK main loop. We will not regain control until gtk_main_quit is called. */
+    gtk_main ();
 
-    // Free resources
-    if (msg != NULL) {
-        gst_message_unref(msg);
-    }
-
-    gst_object_unref(bus);
-    gst_element_set_state(pipeline, GST_STATE_NULL);
-    gst_object_unref(pipeline);
-
+    /* Free resources */
+    gst_element_set_state (data.playbin, GST_STATE_NULL);
+    gst_object_unref (data.playbin);
     free(cfg);
 
     return 0;
-;}
+}
